aaa8ae2e929add74a997df0ea0d89a6f81cf8048
m7mdso3dy <kimosody125@gmail.com>
2021-08-25 13:16:10 +0200

    First Commit

---
+++ jsCode/code.js
@@ -0,0 +1,75 @@
+'use strict'
+//...................variables
+// ..........blog slider variable
+const sliderParentBlog = document.querySelector('.slider-parent');
+const indicatorsBlog = document.querySelectorAll('.indicator-blog');
+const blogSliderWidth = sliderParentBlog.offsetWidth;
+let NumOfblogSlide = 0;
+//events slider variable
+const sliderParentEvent = document.querySelector('.events-slider-parent');
+const indicatorsEvent = document.querySelectorAll('.indicator-event');
+const eventSliderWidth = sliderParentEvent.offsetWidth;
+let NumOfEventSlide = 0;
+//.......................news letter layover
+const subToNews = document.querySelector('.btn-newsletter');
+const exit = document.querySelector('.exit');
+const layover = document.querySelector('.lay1');
+//.....................functions
+//........ blog slider functions
+const handlingScrollingBlog = function (i) {
+    indicatorsBlog[NumOfblogSlide].classList.toggle('active');
+    NumOfblogSlide = i;
+    indicatorsBlog[NumOfblogSlide].classList.toggle('active');
+    sliderParentBlog.scrollTo({
+        left: NumOfblogSlide * blogSliderWidth,
+        behavior:'smooth'
+    })
+}
+
+//.....events slider function 
+const handlingScrollingEvent = function (i) {
+    indicatorsEvent[NumOfEventSlide].classList.toggle('active');
+    NumOfEventSlide = i;
+    indicatorsEvent[NumOfEventSlide].classList.toggle('active');
+    sliderParentEvent.scrollTo({
+        left: NumOfEventSlide * eventSliderWidth,
+        behavior:'smooth'
+    })
+}
+
+
+
+
+//............hading events
+///hadling blog scrolling
+
+indicatorsBlog.forEach((ind,i) => {
+    ind.addEventListener('click', function (e) {
+        this.setAttribute('disabled', true);
+        setTimeout(function () {
+            indicatorsBlog[i].removeAttribute('disabled');
+        }, 800);
+        handlingScrollingBlog(i);
+        
+    })
+})
+
+///hadling event scrolling
+indicatorsEvent.forEach((ind, i) => {
+    ind.addEventListener('click', function (e) {
+        this.setAttribute('disabled', true);
+        setTimeout(function () {
+            indicatorsEvent[i].removeAttribute('disabled');
+        }, 800);
+        handlingScrollingEvent(i);
+
+    })
+})
+
+//hadling newsletter
+subToNews.addEventListener('click', function () {
+    layover.classList.add('active-layover');
+});
+exit.addEventListener('click', function () {
+    layover.classList.remove('active-layover');
+});
