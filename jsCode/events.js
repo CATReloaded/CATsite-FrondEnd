8d64dbad8385a258436838382f9da5a997008ece
m7mdso3dy <kimosody125@gmail.com>
2021-09-19 13:21:12 +0200

    FSBP

--- jsCode/events.js
+++ jsCode/events.js
@@ -5,7 +5,7 @@ const exit = document.querySelector('.exit');
 const layover = document.querySelector('.lay1');
 //hadling newsletter
 subToNews.addEventListener('click', function () {
-    layover.classList.add('active-layover');
+    layover.classList.toggle('active-layover');
 });
 exit.addEventListener('click', function () {
     layover.classList.remove('active-layover');
