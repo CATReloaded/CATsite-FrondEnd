ea5682047b0fadf963e06b2b510c52cf6be7de7a
m7mdso3dy <kimosody125@gmail.com>
2021-09-20 12:11:52 +0200

    finish

--- jsCode/code.js
+++ jsCode/code.js
@@ -1,10 +1,10 @@
 'use strict'
 //...................variables
-// ..........blog slider variable
-const sliderParentBlog = document.querySelector('.slider-parent');
-const indicatorsBlog = document.querySelectorAll('.indicator-blog');
-//const blogSliderWidth = sliderParentBlog.offsetWidth;
-let NumOfblogSlide = 0;
+// ..........projects slider variable
+const sliderParentProjects = document.querySelector('.projects-slider-parent');
+const indicatorsProjects = document.querySelectorAll('.indicator-project');
+const projectsSliderWidth = sliderParentProjects.offsetWidth;
+let NumOfProjectSlide = 0;
 //events slider variable
 const sliderParentEvent = document.querySelector('.events-slider-parent');
 const indicatorsEvent = document.querySelectorAll('.indicator-event');
@@ -15,17 +15,17 @@ const subToNews = document.querySelector('.btn-newsletter');
 const exit = document.querySelector('.exit');
 const layover = document.querySelector('.lay1');
 //.....................functions
-//........ blog slider functions
-/*const handlingScrollingBlog = function (i) {
-    indicatorsBlog[NumOfblogSlide].classList.toggle('active');
-    NumOfblogSlide = i;
-    indicatorsBlog[NumOfblogSlide].classList.toggle('active');
-    sliderParentBlog.scrollTo({
-        left: NumOfblogSlide * blogSliderWidth,
+//........ project slider functions
+const handlingScrollingProject = function (i) {
+    indicatorsProjects[NumOfProjectSlide].classList.toggle('active');
+    NumOfProjectSlide = i;
+    indicatorsProjects[NumOfProjectSlide].classList.toggle('active');
+    sliderParentProjects.scrollTo({
+        left: NumOfProjectSlide * projectsSliderWidth,
         behavior: 'smooth'
     })
 }
-*/
+
 //.....events slider function 
 const handlingScrollingEvent = function (i) {
     indicatorsEvent[NumOfEventSlide].classList.toggle('active');
@@ -41,15 +41,15 @@ const handlingScrollingEvent = function (i) {
 
 
 //............hading events
-///hadling blog scrolling
+///hadling projects scrolling
 
-indicatorsBlog.forEach((ind, i) => {
+indicatorsProjects.forEach((ind, i) => {
     ind.addEventListener('click', function (e) {
         this.setAttribute('disabled', true);
         setTimeout(function () {
-            indicatorsBlog[i].removeAttribute('disabled');
+            indicatorsProjects[i].removeAttribute('disabled');
         }, 800);
-        handlingScrollingBlog(i);
+        handlingScrollingProject(i);
 
     })
 })
